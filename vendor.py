dc72bd4124838397ad69d604c6db13f61f0bff06
Christopher Wilson <cjwilson2020@gmail.com>
2019-10-19 13:41:54 -0500

    Connection between python files and MongoDB Database established vis PyMongo

--- vendor.py
+++ vendor.py
@@ -1,1 +1,15 @@
-import pymongo
+from pymongo import MongoClient
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+client = MongoClient("mongodb+srv://cwilson:Cps41906243@cluster0-lftpp.gcp.mongodb.net/test")
+db=client.cwilson
+# Issue the serverStatus command and print the results
+serverStatusResult=db.command("serverStatus")
+pprint(serverStatusResult)
+
+#class Vendor():
+	
+
+
+
